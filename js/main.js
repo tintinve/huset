e7f42e90fef3ecb3163e9aeb739c7c2fbade884c
tintinve <pedr0261@stud.kea.dk>
2018-04-28 23:34:53 +0200

    V0.07

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let template = document.querySelector("#eventtemp").content;
 let eventlist = document.querySelector("#eventlist")
 let page = 1;
 let lookingForData = false;
+let catLink = "http://tintinve.com/kea/16-cms/wp-json/wp/v2/categories"
+const aside = document.querySelector('aside');
 
 
 function fetchData() {
@@ -24,7 +26,6 @@ function showEvent(anEvent) {
         clone.querySelector("h1").textContent = anEvent.title.rendered;
         clone.querySelector(".description").innerHTML = anEvent.content.rendered;
         clone.querySelector(".price span").textContent = anEvent.acf.price;
-        console.log(anEvent.acf);
         clone.querySelector(".category").textContent = "Category: " + anEvent.acf.event_type;
         clone.querySelector(".venue").textContent = "Location: " + anEvent.acf.location;
         clone.querySelector(".date").textContent = "Date: " + anEvent.acf.date;
@@ -57,4 +58,32 @@ function bottomVisible() {
     const pageHeight = document.documentElement.scrollHeight
     const bottomOfPage = visible + scrollY >= pageHeight
     return bottomOfPage || pageHeight < visible
+}
+
+fetch(catLink).then(result => result.json()).then(cats => sort(cats));
+function sort(cats){
+    cats.forEach(cat => {
+        const contenedor1 = document.createElement("section");
+        const a = document.createElement("a");
+        a.href = "#";
+        a.textContent = cat.name;
+        a.classList.add("menu_item");
+        a.addEventListener('click', () => filter(cat));
+        aside.appendChild(a);
+    })
+}
+function filter(category) {
+    console.log(category.name);
+    
+}
+
+
+function burger(x) {
+    x.classList.toggle("change");
+}
+
+
+document.querySelector(".burger").addEventListener('click', trae_menu);
+function trae_menu(){
+    document.querySelector("aside").classList.toggle("traeMenu");
 }
